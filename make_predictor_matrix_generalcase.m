8e4b9d199fbe6433a1e7321388d28d4fea67ad8a
benengx <35305189+benengx@users.noreply.github.com>
2019-03-07 15:49:11 -0500

    Add files via upload

---
+++ make_predictor_matrix_generalcase.m
@@ -0,0 +1,114 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%    Ben Engelhard, Princeton University (2019).
+%
+%    This program is provided free without any warranty; you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation.
+%    If this code is used, please cite: B Engelhard et al. Specialized coding of sensory, motor, and cognitive variables in midbrain dopamine neurons. Nature, 2019.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%% make_predictor_matrix_generalcase.m
+%%%
+%%% Description: Make the predictor matrix to be used with the process_encoding_model function
+%
+% arguments: base_variables - a cell array where each term is a base variable which can be either a cell array or a vector. in both cases the division is by trials in a session. For a cell array base variable, 
+%                             each term is a vector that corresponds to one trial in the session, and include the base variable's values in all timepoints of that trial. A vector base variable is necessarily a
+%                             'whole-trial' variable where each term is '0' or '1' and denotes the value of that variable for all timepoints in that trial. At least one base variable needs to be a cell array.          
+%            var_types      - a cell array with the same length as the 'base_variables' argument where each term denotes the type of base variable. possible types are: 
+%                             'event'      : a binary base variable where values of '1' denote the occurence of the event variable.
+%                             'whole-trial': a binary bas e variable that takes the same value in all timepoints of a given trial, corrsponding to wether the variable's occurence was valid in that trial. 
+%                             'continuous' : a base variable that takes different values at different timepoints.
+%            groupings      - a cell array that defines how the base variables are grouped into the behavioral variables used for subsequent processing in the encoding model. each term is a vector of
+%                             indices (corresponding to the base_variables argument). All indices in one term of the cell arary are considered to belong to the same variable when processing the encoding model.
+%                             Currently, only base variables of the same type can be grouped.
+%
+% outputs:   pred_allmat       - cell array corresponding to a matrix of predictors, each term corresponds to one trial where rows are timepoints and columns are the different predictors
+%            pred_inds_cell    - cell array where each term has a vector of indices of the predictors that belong to a specific behavioral variable
+%            grouped_var_types - cell array where each term has the type ('event','whole-trial', or 'continuous') of the corresponding behavioral variable. if no value is given, then each base variable is 
+%                                considered to be a behavioral variable
+
+function [pred_allmat,pred_inds_cell,grouped_var_types] = make_predictor_matrix_generalcase(base_variables,var_types,groupings)
+
+if nargin<3
+    groupings = mat2cell(1:length(base_variables),1,ones(1,length(base_variables)));
+end
+
+num_base_vars = length(base_variables);
+numtrials = length(base_variables{1});
+load('spline_basis30_int.mat'); % this results in the variable 'spline_basis' which is used for event variables. It is curently built using 7 splines and 30 timepoints. 
+                                % change this if you want a different spline basis set.
+
+% find the first cell array base variable to get from it the number of timepoints in each trial
+num_points_per_trial = zeros(numtrials,1);
+found_cellarraybasevar = 0;
+for basevarctr = 1:num_base_vars
+    if iscell(base_variables{basevarctr})
+        for k=1:numtrials
+            num_points_per_trial(k) = length(base_variables{basevarctr}{k});
+        end
+        found_cellarraybasevar = 1;
+        continue
+    end
+end
+if ~found_cellarraybasevar
+    error('At least one base variable needs to be a cell array. If necessary, convert a whole trial-variable to a cell array variable')
+end
+
+for k=1:numtrials
+    clear pred_curmat
+    
+    % process event variables
+    event_vars_inds = find_non_empty_cells(strfind(var_types,'event'));
+    
+    for varctr = 1:length(event_vars_inds)
+        clear cur_event_var
+        for spctr = 1:size(spline_basis,2)
+            w = conv(base_variables{event_vars_inds(varctr)}{k} ,spline_basis(:,spctr));
+            cur_event_var(:,spctr) = w(1:length(base_variables{event_vars_inds(varctr)}{k}));
+        end
+        pred_curmat{1,event_vars_inds(varctr)} = cur_event_var;
+    end
+
+    % process whole-trial variables. they can be either vectors or cell arrays
+    wholetrial_vars_inds = find_non_empty_cells(strfind(var_types,'whole-trial'));
+    
+    for varctr = 1:length(wholetrial_vars_inds)
+        if iscell(base_variables{wholetrial_vars_inds(varctr)})
+            cur_wt_var = base_variables{wholetrial_vars_inds(varctr)}{k};
+        else
+            cur_wt_var  = zeros(num_points_per_trial(k),1) + base_variables{wholetrial_vars_inds(varctr)}(k);
+        end
+        pred_curmat{1,wholetrial_vars_inds(varctr)} = cur_wt_var;
+    end
+
+    % process continuous variables. they can be either vectors or cell arrays
+    cont_vars_inds = find_non_empty_cells(strfind(var_types,'continuous'));
+    
+    for varctr = 1:length(cont_vars_inds)
+        pred_curmat{1,cont_vars_inds(varctr)} = base_variables{cont_vars_inds(varctr)}{k};
+    end
+
+    pred_allmat{k,1} = cell2mat(pred_curmat);
+    
+    % get the indices of the predictors coresponding to the different variables
+    if k==1
+        for varctr = 1:size(pred_curmat,2)
+           preds_inds_basevars{varctr} = (1:size(pred_curmat{1,varctr},2)) + size(cell2mat(pred_curmat(1,1:varctr-1)),2);
+        end
+        for grpctr = 1:length(groupings)
+            pred_inds_cell{grpctr} = cell2mat(preds_inds_basevars(groupings{grpctr}));
+            cur_types = var_types(groupings{grpctr});
+            grouped_var_types{grpctr} = cur_types{1};
+            
+            if ~isempty(setdiff(cur_types,cur_types{1}))
+               error('Grouped variables must have the same type') 
+            end
+        end
+    end
+    
+end
+
+
+
+
+
---
+++ process_encoding_model.m
@@ -0,0 +1,154 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%    Ben Engelhard, Princeton University (2019).
+%
+%    This program is provided free without any warranty; you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation.
+%    If this code is used, please cite: B Engelhard et al. Specialized coding of sensory, motor, and cognitive variables in midbrain dopamine neurons. Nature, 2019.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%% process_encoding_model.m
+%%%
+%%% Description: Process the encoding model on a matrix of predictors belonging to different behavioral variables and a matrix of neural activity traces correspoding to one or more neurons. return the relative
+%%% contribution of each behavioral variable to each neuron and the F-statistic derived from a nested model comparison for each behavioral variable and neuron to determine if the behavioral variable is 
+%%% significantly represented in the neuron's activity.
+%
+% arguments: pred_allmat      - cell array correspoding to a matrix of behavioral predictors, each term is a trial and contains a matrix where rows are timepoints and columns are behavioral predictors.
+%            pred_inds_cell   - cell array where each term has a vector of indices of the predictors that belong to a specific behavioral variable
+%            neural_act_mat   - cell array correspoding to a matrix of activity traces, each term is a trial and contains a matrix where rows are timepoints and columns are traces correspoding to different
+%                               neurons. Timepoints where the neuronal activity is not defined (e.g. a the imaging became unstable) are filled with NaNs (Currently it is assumed that before the first NaN
+%                               activity was always defined, and after the first NaN activity is never defined).
+%            pred_types_cell  - cell array where each terms indicates the type of behavioral variable ('event', 'whole-trial', or 'continuous').
+%            approach         - 'norefit': calculate regression weights with the full model, then zero the weights correspoding to the predictors being dropped. 'refit' : calculate regression weights
+%                               without the weights correspoding to the predictors being dropped (partial model).
+%
+% outputs:   relative_contrib - a matrix where rows are behavioral variables and columns are neurons. each term is the relative contribution of the behavioral variable to the neural activity.
+%            Fstat_mat        - a matrix where rows are behavioral variables and columns are neurons. each term is the F-statistic associated with the nested model comparison where the predicted variable is the
+%                               activity of the neuron, the full model is the model containing the predictors from al behavioral variables, and the partial model is the model containing the predictors from all
+%                               variables except the one being tested. The value of this statistic shoudl be compared to a distirbution of staitsitc values obtained by erforing the same oprateion on shuffled 
+%                               data, directly using the p-value assocaited with the staitstic is not valid given the autocorrelations in the data.
+
+function [relative_contrib,Fstat_mat] = process_encoding_model(pred_allmat, pred_inds_cell, neural_act_mat, pred_types_cell,approach)
+
+numcells = size(neural_act_mat{1},2);
+numtrials_all = length(pred_allmat);
+
+% find for each neuron trials where activity is defined, and also the length of each trial
+defined_mat = zeros(numtrials_all,numcells);
+trial_length_vec = zeros(numtrials_all,1);
+
+for trctr=1:numtrials_all
+    defined_mat(trctr,:) = ~sum(isnan(neural_act_mat{trctr}));
+    trial_length_vec (trctr) = size(neural_act_mat{trctr},1);
+end
+
+
+% use crossvalidation to find the best polynomial degree to apply for the continuous variables
+num_cv_folds = 5;
+max_poly_deg = 3;
+
+% rng(0,'twister')
+full_R2_vec = zeros(numcells,1);
+partial_R2_vec = zeros(numcells,length(pred_inds_cell));
+relative_contrib = zeros(numcells,length(pred_inds_cell));
+for cellctr = 1:numcells
+    cur_good_trials = 1:find(defined_mat(:,cellctr),1,'last');
+    num_trials_per_fold = ceil(length(cur_good_trials)/num_cv_folds);
+    
+    temp_neural_act = cell2mat(neural_act_mat(cur_good_trials));
+    cur_neural_act_mat = mat2cell(temp_neural_act(:, cellctr),trial_length_vec(cur_good_trials),1);
+    
+    % zscore the predictors
+    cur_pred_allmat_z = mat2cell(zscore(cell2mat(pred_allmat(cur_good_trials))),trial_length_vec(cur_good_trials),size(pred_allmat{1},2));
+    rng('default')
+    cur_random_vector = randperm(length(cur_good_trials));
+    
+    % get indices of test and train trials for CV
+    for foldctr = 1:num_cv_folds
+        kf_inds{foldctr} = num_trials_per_fold*(foldctr-1)+1:min(num_trials_per_fold*foldctr,length(cur_good_trials));
+        test_trials_folds{foldctr} = cur_random_vector(kf_inds{foldctr});
+        train_trials_folds{foldctr} = setdiff(cur_random_vector,test_trials_folds{foldctr});
+    end
+    
+    [~,F_vec] = get_f_pvals_reg(cell2mat(cur_pred_allmat_z),zscore(cell2mat(cur_neural_act_mat)),pred_inds_cell);
+    Fstat_mat(cellctr,:) = F_vec;
+    
+    % make matrix of all possible combinations of polynomial degrees for all continuous variables
+    cont_inds = find_non_empty_cells(strfind(pred_types_cell,'continuous'));
+    non_cont_inds = setdiff(1:length(pred_types_cell),cont_inds);
+    num_cont_inds = length(cont_inds);
+    all_degs_mat = [];
+    clear cur_cont_preds_inds cur_base_preds
+    temp_predmat = cell2mat(cur_pred_allmat_z);
+    for cont_var_ctr = 1:num_cont_inds
+        all_degs_mat  = ceil([all_degs_mat mod((1:max_poly_deg^num_cont_inds)/(max_poly_deg^(cont_var_ctr-1)),max_poly_deg+.01)']);
+        cur_cont_preds_inds{cont_var_ctr} = pred_inds_cell{cont_inds(cont_var_ctr)};
+        cur_base_preds{cont_var_ctr} = temp_predmat(:,cur_cont_preds_inds{cont_var_ctr}); % predictors for the current continuous variable before adding any additional polynomial degrees
+    end
+    all_cont_pred_inds = cell2mat(cur_cont_preds_inds);
+    all_non_cont_pred_inds = setdiff(1:size(cur_pred_allmat_z{1},2),all_cont_pred_inds);
+    non_cont_predmat = temp_predmat(:,all_non_cont_pred_inds);
+    
+    
+    deg_R2_vec = zeros(1,size(all_degs_mat,1));
+    for degctr = 1:size(all_degs_mat,1)
+        full_predmat = non_cont_predmat;
+        for cont_var_ctr = 1:num_cont_inds
+            cur_cont_pred_add = [];
+            for curdegctr = 1:all_degs_mat(degctr,cont_var_ctr)
+                cur_cont_pred_add = [cur_cont_pred_add cur_base_preds{cont_var_ctr}.^curdegctr];
+            end
+            full_predmat = [full_predmat cur_cont_pred_add];
+        end
+        
+        %get CV R2 for this predictor matrix
+        full_predmat_cell = mat2cell(full_predmat,trial_length_vec(cur_good_trials),size(full_predmat,2));
+        deg_R2_vec(degctr) = get_CV_R2(full_predmat_cell,cur_neural_act_mat,test_trials_folds,train_trials_folds,trial_length_vec(cur_good_trials));
+    end
+    
+    [cur_full_R2,best_deg_ind] = max(deg_R2_vec);
+    full_R2_vec(cellctr,1) = cur_full_R2;
+    
+    % now make the matrix with the optimal poly degree for each continuous variable and update the predictor indices as well
+    clear pred_inds_cell_new
+    full_predmat = [];
+    pred_inds_cell_new = {};
+    new_pred_inds_ctr = 1;
+    for non_cont_var_ctr = 1:length(non_cont_inds)
+        cur_predmatvar = temp_predmat(:,cell2mat(pred_inds_cell(non_cont_inds(non_cont_var_ctr))));
+        cur_num_preds = size(full_predmat ,2);
+        full_predmat = [full_predmat cur_predmatvar];
+        pred_inds_cell_new{non_cont_inds(non_cont_var_ctr)} = (1:size(cur_predmatvar,2))+cur_num_preds ;
+    end
+    for cont_var_ctr = 1:num_cont_inds
+        all_cont_pred_add{cont_var_ctr} = [];
+        for curdegctr = 1:all_degs_mat(best_deg_ind,cont_var_ctr)
+            all_cont_pred_add{cont_var_ctr} = [all_cont_pred_add{cont_var_ctr} cur_base_preds{cont_var_ctr}.^curdegctr];
+        end
+        cur_num_preds = size(full_predmat ,2);
+        full_predmat = [full_predmat zscore(all_cont_pred_add{cont_var_ctr})];
+        pred_inds_cell_new{cont_inds(cont_var_ctr)} = (1:size(all_cont_pred_add{cont_var_ctr},2))+cur_num_preds ;
+    end
+    
+    full_predmat_cell = mat2cell(full_predmat,trial_length_vec(cur_good_trials),size(full_predmat,2));
+    
+    
+    % now calculate the relative contributions.  first calculate the R2 when each variable is omitted.
+    
+    if ~isempty(full_R2_vec(cellctr,1))
+        full_R2_vec(cellctr,1) = get_CV_R2(full_predmat_cell,cur_neural_act_mat,test_trials_folds,train_trials_folds,trial_length_vec(cur_good_trials));
+    end
+    for varctr = 1:length(pred_inds_cell_new)
+        partial_R2_vec(cellctr,varctr) =  get_CV_R2(full_predmat_cell,cur_neural_act_mat,test_trials_folds,train_trials_folds,trial_length_vec(cur_good_trials),cell2mat(pred_inds_cell_new(varctr)),approach);
+    end
+    
+    cur_R2_diff = (full_R2_vec(cellctr,1) - partial_R2_vec(cellctr,:))/full_R2_vec(cellctr,1);
+    cur_R2_diff(cur_R2_diff<0)=0;
+    cur_R2_diff(cur_R2_diff>1)=1;
+    relative_contrib(cellctr,:) = cur_R2_diff/sum(cur_R2_diff);
+    
+    
+end
+
+
+
