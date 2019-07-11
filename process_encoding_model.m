260888ed60dcdf07621c637d5a11410428e9b40b
benengx <35305189+benengx@users.noreply.github.com>
2019-07-11 01:41:35 -0400

    Add files via upload

--- process_encoding_model.m
+++ process_encoding_model.m
@@ -21,14 +21,28 @@
 %            pred_types_cell  - cell array where each terms indicates the type of behavioral variable ('event', 'whole-trial', or 'continuous').
 %            approach         - 'norefit': calculate regression weights with the full model, then zero the weights correspoding to the predictors being dropped. 'refit' : calculate regression weights
 %                               without the weights correspoding to the predictors being dropped (partial model).
+%            trial_types_to_match - If it is desired to match the effects on the regression of trials of different types, then this argument should be a vector the same length as pred_allmat where each term is 
+%                                   a number denoting trial type. 
 %
 % outputs:   relative_contrib - a matrix where rows are behavioral variables and columns are neurons. each term is the relative contribution of the behavioral variable to the neural activity.
 %            Fstat_mat        - a matrix where rows are behavioral variables and columns are neurons. each term is the F-statistic associated with the nested model comparison where the predicted variable is the
 %                               activity of the neuron, the full model is the model containing the predictors from al behavioral variables, and the partial model is the model containing the predictors from all
 %                               variables except the one being tested. The value of this statistic shoudl be compared to a distirbution of staitsitc values obtained by erforing the same oprateion on shuffled 
 %                               data, directly using the p-value assocaited with the staitstic is not valid given the autocorrelations in the data.
+%            full_R2_vec      - a vector indicating the R2 of the full model for each neuron.
+%            B_all            - cell array with predictor weights for each neuron calculated by performing the regression on all data (without crossvalidation).
+%            pred_inds_cell_opt - cell array where each term corresponds to a neuron and contains a cell array where each term has a vector of indices of the predictors that belong to a specific behavioral 
+%                                 variable, updated to reflect the optimal model for the given neuron.
 
-function [relative_contrib,Fstat_mat] = process_encoding_model(pred_allmat, pred_inds_cell, neural_act_mat, pred_types_cell,approach)
+function [relative_contrib,Fstat_mat,full_R2_vec,predicted_gcamp,B_all,pred_inds_cell_opt] = process_encoding_model(pred_allmat, pred_inds_cell, neural_act_mat, pred_types_cell,approach, trial_types_to_match)
+if nargin<5
+    approach = 'norefit';
+end
+if nargin<6
+    trial_types_to_match = [];
+end
+
+neural_act_mat=neural_act_mat(:); % make it a column cell array in case it's not
 
 numcells = size(neural_act_mat{1},2);
 numtrials_all = length(pred_allmat);
@@ -54,7 +68,9 @@ relative_contrib = zeros(numcells,length(pred_inds_cell));
 for cellctr = 1:numcells
     cur_good_trials = 1:find(defined_mat(:,cellctr),1,'last');
     num_trials_per_fold = ceil(length(cur_good_trials)/num_cv_folds);
-    
+    if isempty(cur_good_trials)
+        continue
+    end
     temp_neural_act = cell2mat(neural_act_mat(cur_good_trials));
     cur_neural_act_mat = mat2cell(temp_neural_act(:, cellctr),trial_length_vec(cur_good_trials),1);
     
@@ -80,66 +96,80 @@ for cellctr = 1:numcells
     all_degs_mat = [];
     clear cur_cont_preds_inds cur_base_preds
     temp_predmat = cell2mat(cur_pred_allmat_z);
-    for cont_var_ctr = 1:num_cont_inds
-        all_degs_mat  = ceil([all_degs_mat mod((1:max_poly_deg^num_cont_inds)/(max_poly_deg^(cont_var_ctr-1)),max_poly_deg+.01)']);
-        cur_cont_preds_inds{cont_var_ctr} = pred_inds_cell{cont_inds(cont_var_ctr)};
-        cur_base_preds{cont_var_ctr} = temp_predmat(:,cur_cont_preds_inds{cont_var_ctr}); % predictors for the current continuous variable before adding any additional polynomial degrees
-    end
-    all_cont_pred_inds = cell2mat(cur_cont_preds_inds);
-    all_non_cont_pred_inds = setdiff(1:size(cur_pred_allmat_z{1},2),all_cont_pred_inds);
-    non_cont_predmat = temp_predmat(:,all_non_cont_pred_inds);
     
-    
-    deg_R2_vec = zeros(1,size(all_degs_mat,1));
-    for degctr = 1:size(all_degs_mat,1)
-        full_predmat = non_cont_predmat;
+    if num_cont_inds>0
         for cont_var_ctr = 1:num_cont_inds
-            cur_cont_pred_add = [];
-            for curdegctr = 1:all_degs_mat(degctr,cont_var_ctr)
-                cur_cont_pred_add = [cur_cont_pred_add cur_base_preds{cont_var_ctr}.^curdegctr];
+            all_degs_mat  = ceil([all_degs_mat mod((1:max_poly_deg^num_cont_inds)/(max_poly_deg^(cont_var_ctr-1)),max_poly_deg+.01)']);
+            cur_cont_preds_inds{cont_var_ctr} = pred_inds_cell{cont_inds(cont_var_ctr)};
+            cur_base_preds{cont_var_ctr} = temp_predmat(:,cur_cont_preds_inds{cont_var_ctr}); % predictors for the current continuous variable before adding any additional polynomial degrees
+        end
+        all_cont_pred_inds = cell2mat(cur_cont_preds_inds);
+        all_non_cont_pred_inds = setdiff(1:size(cur_pred_allmat_z{1},2),all_cont_pred_inds);
+        non_cont_predmat = temp_predmat(:,all_non_cont_pred_inds);
+        
+        
+        deg_R2_vec = zeros(1,size(all_degs_mat,1));
+        for degctr = 1:size(all_degs_mat,1)
+            full_predmat = non_cont_predmat;
+            for cont_var_ctr = 1:num_cont_inds
+                cur_cont_pred_add = [];
+                for curdegctr = 1:all_degs_mat(degctr,cont_var_ctr)
+                    cur_cont_pred_add = [cur_cont_pred_add cur_base_preds{cont_var_ctr}.^curdegctr];
+                end
+                full_predmat = [full_predmat cur_cont_pred_add];
             end
-            full_predmat = [full_predmat cur_cont_pred_add];
+            
+            %get CV R2 for this predictor matrix
+            full_predmat_cell = mat2cell(full_predmat,trial_length_vec(cur_good_trials),size(full_predmat,2));
+            if nargout<5
+                [cur_R2,cur_predicted] = get_CV_R2(full_predmat_cell,cur_neural_act_mat,test_trials_folds,train_trials_folds,trial_length_vec(cur_good_trials),[],approach,trial_types_to_match);
+            else
+                [cur_R2,cur_predicted,curB] = get_CV_R2(full_predmat_cell,cur_neural_act_mat,test_trials_folds,train_trials_folds,trial_length_vec(cur_good_trials),[],approach,trial_types_to_match);
+                weights_reg_cell{degctr} = curB;                
+            end
+            deg_R2_vec(degctr) = cur_R2;
+            deg_predicted_cell{degctr} = cur_predicted;
         end
         
-        %get CV R2 for this predictor matrix
-        full_predmat_cell = mat2cell(full_predmat,trial_length_vec(cur_good_trials),size(full_predmat,2));
-        deg_R2_vec(degctr) = get_CV_R2(full_predmat_cell,cur_neural_act_mat,test_trials_folds,train_trials_folds,trial_length_vec(cur_good_trials));
-    end
-    
-    [cur_full_R2,best_deg_ind] = max(deg_R2_vec);
-    full_R2_vec(cellctr,1) = cur_full_R2;
-    
-    % now make the matrix with the optimal poly degree for each continuous variable and update the predictor indices as well
-    clear pred_inds_cell_new
-    full_predmat = [];
-    pred_inds_cell_new = {};
-    new_pred_inds_ctr = 1;
-    for non_cont_var_ctr = 1:length(non_cont_inds)
-        cur_predmatvar = temp_predmat(:,cell2mat(pred_inds_cell(non_cont_inds(non_cont_var_ctr))));
-        cur_num_preds = size(full_predmat ,2);
-        full_predmat = [full_predmat cur_predmatvar];
-        pred_inds_cell_new{non_cont_inds(non_cont_var_ctr)} = (1:size(cur_predmatvar,2))+cur_num_preds ;
-    end
-    for cont_var_ctr = 1:num_cont_inds
-        all_cont_pred_add{cont_var_ctr} = [];
-        for curdegctr = 1:all_degs_mat(best_deg_ind,cont_var_ctr)
-            all_cont_pred_add{cont_var_ctr} = [all_cont_pred_add{cont_var_ctr} cur_base_preds{cont_var_ctr}.^curdegctr];
+        [cur_full_R2,best_deg_ind] = max(deg_R2_vec);
+        full_R2_vec(cellctr,1) = cur_full_R2;
+        predicted_gcamp{cellctr} = deg_predicted_cell{best_deg_ind};
+        if nargout>=5
+            B_all{cellctr} = weights_reg_cell{best_deg_ind};
         end
-        cur_num_preds = size(full_predmat ,2);
-        full_predmat = [full_predmat zscore(all_cont_pred_add{cont_var_ctr})];
-        pred_inds_cell_new{cont_inds(cont_var_ctr)} = (1:size(all_cont_pred_add{cont_var_ctr},2))+cur_num_preds ;
+        % now make the matrix with the optimal poly degree for each continuous variable and update the predictor indices as well
+        clear pred_inds_cell_new
+        full_predmat = [];
+        pred_inds_cell_new = {};
+        new_pred_inds_ctr = 1;
+        for non_cont_var_ctr = 1:length(non_cont_inds)
+            cur_predmatvar = temp_predmat(:,cell2mat(pred_inds_cell(non_cont_inds(non_cont_var_ctr))));
+            cur_num_preds = size(full_predmat ,2);
+            full_predmat = [full_predmat cur_predmatvar];
+            pred_inds_cell_new{non_cont_inds(non_cont_var_ctr)} = (1:size(cur_predmatvar,2))+cur_num_preds ;
+        end
+        for cont_var_ctr = 1:num_cont_inds
+            all_cont_pred_add{cont_var_ctr} = [];
+            for curdegctr = 1:all_degs_mat(best_deg_ind,cont_var_ctr)
+                all_cont_pred_add{cont_var_ctr} = [all_cont_pred_add{cont_var_ctr} cur_base_preds{cont_var_ctr}.^curdegctr];
+            end
+            cur_num_preds = size(full_predmat ,2);
+            full_predmat = [full_predmat zscore(all_cont_pred_add{cont_var_ctr})];
+            pred_inds_cell_new{cont_inds(cont_var_ctr)} = (1:size(all_cont_pred_add{cont_var_ctr},2))+cur_num_preds ;
+        end
+        
+        full_predmat_cell = mat2cell(full_predmat,trial_length_vec(cur_good_trials),size(full_predmat,2));
+        
+    else
+        full_predmat_cell = cur_pred_allmat_z;
+        full_R2_vec(cellctr,1) = get_CV_R2(full_predmat_cell,cur_neural_act_mat,test_trials_folds,train_trials_folds,trial_length_vec(cur_good_trials),[],approach,trial_types_to_match);
+        pred_inds_cell_new = pred_inds_cell;
     end
-    
-    full_predmat_cell = mat2cell(full_predmat,trial_length_vec(cur_good_trials),size(full_predmat,2));
-    
-    
+    pred_inds_cell_opt{cellctr} = pred_inds_cell_new;
     % now calculate the relative contributions.  first calculate the R2 when each variable is omitted.
     
-    if ~isempty(full_R2_vec(cellctr,1))
-        full_R2_vec(cellctr,1) = get_CV_R2(full_predmat_cell,cur_neural_act_mat,test_trials_folds,train_trials_folds,trial_length_vec(cur_good_trials));
-    end
     for varctr = 1:length(pred_inds_cell_new)
-        partial_R2_vec(cellctr,varctr) =  get_CV_R2(full_predmat_cell,cur_neural_act_mat,test_trials_folds,train_trials_folds,trial_length_vec(cur_good_trials),cell2mat(pred_inds_cell_new(varctr)),approach);
+        partial_R2_vec(cellctr,varctr) =  get_CV_R2(full_predmat_cell,cur_neural_act_mat,test_trials_folds,train_trials_folds,trial_length_vec(cur_good_trials),cell2mat(pred_inds_cell_new(varctr)),approach,trial_types_to_match);
     end
     
     cur_R2_diff = (full_R2_vec(cellctr,1) - partial_R2_vec(cellctr,:))/full_R2_vec(cellctr,1);
